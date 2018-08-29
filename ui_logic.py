df4c9c349e59bc3c8dfb9e94b4894f4571aeb4d4
zylo117 <zylo117@users.noreply.github.com>
2018-08-29 14:53:49 +0800

    update

--- ui_logic.py
+++ ui_logic.py
@@ -50,6 +50,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # init painter
         self.draw = 'point'
         self.points = []
+        self.last_box = []
 
     def load_next(self, previous=False):
         if previous:
@@ -71,12 +72,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def load_pic(self, img_path):
         self.current_img = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        self.current_img = cv2.cvtColor(self.current_img, cv2.COLOR_BGR2RGB)
 
     def set_pic(self, img=None):
         if img is None:
             img = self.current_img
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.pic_real_h, self.pic_real_w = img.shape[:2]
 
         # adapt window
@@ -85,48 +86,103 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         video_ratio = self.pic_real_w / self.pic_real_h
         # considering calculation precision error issue, don't use "!=" to judge
         if monitor_ratio - video_ratio > 0.1:
-            image = imutils.resize(img, height=monitor_height, inter=cv2.INTER_AREA)
-            height, width, channel = image.shape
+            height, width, channel = img.shape
             pile_width = channel * width
-            q_image = QImage(image.data, width, height, pile_width, QImage.Format_RGB888)
+            q_image = QImage(img.data, width, height, pile_width, QImage.Format_RGB888)
             qpix = QPixmap.fromImage(q_image).scaled(width, height, Qt.KeepAspectRatio)
         elif monitor_ratio - video_ratio < 0.1:
-            image = imutils.resize(img, width=monitor_width, inter=cv2.INTER_AREA)
-            height, width, channel = image.shape
+            height, width, channel = img.shape
             pile_width = channel * width
-            q_image = QImage(image.data, width, height, pile_width, QImage.Format_RGB888)
+            q_image = QImage(img.data, width, height, pile_width, QImage.Format_RGB888)
             qpix = QPixmap.fromImage(q_image).scaled(width, height, Qt.KeepAspectRatio)
 
         self.pic.setPixmap(qpix)
 
+    def draw_on_pic(self):
+        self.current_img_mask = self.current_img
+        cv2.drawMarker(self.current_img_mask, (int(self.pic_real_x), int(self.pic_real_y)), (255, 0, 0),
+                       cv2.MARKER_CROSS, 11)
+
+        if len(self.points) > 1:
+            cv2.line(self.current_img_mask, (self.points[-1][0], self.points[-1][1]),
+                     (self.points[-2][0], self.points[-2][1]), (0, 255, 0), 1)
+
+        self.set_pic(self.current_img_mask)
+
+    def close_draw(self):
+        if len(self.points) > 2:
+            cv2.line(self.current_img_mask, (self.points[-1][0], self.points[-1][1]),
+                     (self.points[0][0], self.points[0][1]), (0, 255, 0), 1)
+
+            # summary roi data
+            points = np.array(self.points)
+            min_x = np.min(points[:, 0])
+            max_x = np.max(points[:, 0])
+            min_y = np.min(points[:, 1])
+            max_y = np.max(points[:, 1])
+
+            # cv2.rectangle(self.current_img_mask, (min_x, min_y), (max_x, max_y), (0, 0, 255), 1)
+            self.last_box = [min_x, min_y, max_x - min_x, max_y - min_y]
+
+            self.set_pic(self.current_img_mask)
+            self.points = []
+
+            # write to JSON
+
+    def copy_box(self):
+        if len(self.last_box) > 0:
+            # min_x, min_y, max_x, max_y = self.last_box[0], self.last_box[1], self.last_box[2], self.last_box[3]
+            # min_x
+            # cv2.rectangle(self.current_img_mask, , (0, 0, 255), 1)
+            self.set_pic(self.current_img_mask)
+
     def paintEvent(self, event):
-        self.painter = QPainter()
+        self.painter = QPainter(self.pic)
         self.painter.begin(self)
         pen = QPen(Qt.red, 2, Qt.SolidLine)
         self.painter.setPen(pen)
 
         if self.draw == 'point':
             self.painter.drawPoint(self.mouse_window_x, self.mouse_window_y)
+        elif self.draw == 'line':
+            if len(self.points) > 1:
+                self.painter.drawLine(self.points[-1][0], self.points[-1][1], self.points[-2][0], self.points[-2][1])
 
         self.painter.end()
 
+    def check_legal_mouse_co(self):
+        if self.pic_real_x >= 0 and self.pic_real_y >= 0:
+            return True
+        else:
+            return False
+
     def mousePressEvent(self, e):
-        # 左键按下
-        if e.buttons() == QtCore.Qt.LeftButton:
-            print('左')
-        # 右键按下
-        elif e.buttons() == QtCore.Qt.RightButton:
-            print('右')
-        # 中键按下
-        elif e.buttons() == QtCore.Qt.MidButton:
-            print('中')
+        if self.check_legal_mouse_co():
+            # 左键按下
+            if e.buttons() == QtCore.Qt.LeftButton:
+                print('左')
+                self.points.append([int(self.pic_real_x), int(self.pic_real_y)])
+                print(self.points)
+                self.draw_on_pic()
+
+            # 右键按下
+            elif e.buttons() == QtCore.Qt.RightButton:
+                print('右')
+                # copy box roi
+                self.copy_box()
+
+
+            # 中键按下
+            elif e.buttons() == QtCore.Qt.MidButton:
+                print('中')
 
     def mouseDoubleClickEvent(self, e):
-        # 左键双击
-        if e.buttons() == QtCore.Qt.LeftButton:
-            self.points.append([self.mouse_window_x, self.mouse_window_y])
-            print(self.points)
-            self.update()
+        if self.check_legal_mouse_co():
+            # 左键双击
+            if e.buttons() == QtCore.Qt.LeftButton:
+                # self.update()
+                print('左双')
+                self.close_draw()
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Escape:
@@ -143,9 +199,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             else:
                 pass  # do other stuff
         return QMainWindow.eventFilter(self, source, event)
-
-    def resizeEvent(self, event):
-        return QMainWindow.resizeEvent(self, event)
 
     def load_img(self):
         self.image_paths = self.file_select_dialog('选择图片')
@@ -187,18 +240,24 @@ class MousePos(QThread):
         pic_w = mainWindow.pic.width()
         pic_h = mainWindow.pic.height()
 
-        if (mouse_x != self.old_x or mouse_y != self.old_y) and (
-                pic_x < mouse_x < pic_w + pic_x and pic_y < mouse_y < pic_h + pic_y):
-            self.old_x = mouse_x
-            self.old_y = mouse_y
+        if mouse_x != self.old_x or mouse_y != self.old_y:
+            if pic_x < mouse_x < pic_w + pic_x and pic_y < mouse_y < pic_h + pic_y:
+                self.old_x = mouse_x
+                self.old_y = mouse_y
 
-            monitor_x = mouse_x - pic_x
-            monitor_y = mouse_y - pic_y
+                monitor_x = mouse_x - pic_x
+                monitor_y = mouse_y - pic_y
 
-            monitor_x *= mainWindow.pic_real_w / pic_w
-            monitor_y *= mainWindow.pic_real_h / pic_h
+                monitor_x *= mainWindow.pic_real_w / pic_w
+                monitor_y *= mainWindow.pic_real_h / pic_h
 
-            print(monitor_x, monitor_y)
+                mainWindow.pic_real_x = monitor_x
+                mainWindow.pic_real_y = monitor_y
+
+                print(monitor_x, monitor_y)
+            else:
+                mainWindow.pic_real_x = -1
+                mainWindow.pic_real_y = -1
 
 
 # setup GUI
